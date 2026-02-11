cc3ba14bc260b9b57eb1116d7c8ebabf9ce8a4c7
Gabriel Furtado Noll <nollfrt@gmail.com>
2026-02-10 23:08:28 -0500

    implement service layer inside EmployeeService

---
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.challenge.api.service;
+
+import com.challenge.api.dto.CreateEmployeeRequest;
+import com.challenge.api.model.Employee;
+import com.challenge.api.model.EmployeeImpl;
+import com.challenge.api.repository.EmployeeRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class EmployeeService {
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return repository.allEmployees();
+    }
+
+    public Employee getEmployeeByUuid(UUID uuid) {
+        //TODO create exception to use here
+        return null;
+    }
+
+    public Employee createEmployee(CreateEmployeeRequest request) {
+        EmployeeImpl employee = populateEmployeeFromRequest(request);
+
+        // Set UUID (not checking for collisions since chance is EXTREMELY low
+        employee.setUuid(UUID.randomUUID());
+
+        return repository.save(employee);
+    }
+
+    private EmployeeImpl populateEmployeeFromRequest(CreateEmployeeRequest request) {
+        EmployeeImpl employee = new EmployeeImpl();
+
+        // populate employee with information inside the request
+        employee.setFirstName(request.getFirstName());
+        employee.setLastName(request.getLastName());
+        employee.setSalary(request.getSalary());
+        employee.setAge(request.getAge());
+        employee.setJobTitle(request.getJobTitle());
+        employee.setEmail(request.getEmail());
+        employee.setContractHireDate(request.getContractHireDate());
+
+        return employee;
+    }
+}
