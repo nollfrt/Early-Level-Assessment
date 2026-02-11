3acadd08a2aad7caa64340364ddd3b25cfd6caef
Gabriel Furtado Noll <nollfrt@gmail.com>
2026-02-10 22:17:53 -0500

    create CreateEmployeeRequest dto to be used when creating an employee

---
+++ api/src/main/java/com/challenge/api/dto/CreateEmployeeRequest.java
@@ -0,0 +1,69 @@
+package com.challenge.api.dto;
+
+import java.time.Instant;
+
+public class CreateEmployeeRequest {
+    private String firstName;
+    private String lastName;
+    private Integer salary;
+    private Integer age;
+    private String jobTitle;
+    private String email;
+    private Instant contractHireDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getContractHireDate() {
+        return contractHireDate;
+    }
+
+    public void setContractHireDate(Instant contractHireDate) {
+        this.contractHireDate = contractHireDate;
+    }
+}
